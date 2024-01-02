3eb58add43ff1284e77666dbcb0c607425d2ce05
Ciorba Antonio <144670129+Mistereptil09@users.noreply.github.com>
2024-01-02 17:14:38 +0100

    Delete Compteur directory

--- Compteur/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <ul> <!-- those are false informations -->
-        <li>Adresse : 20 bis jardins Boildieu, 92800 Puteaux</li>
-        <li>Telephone : 00 00 00 00 00</li>
-        <li>Email : compteur@oui.fr</li>
-        <li>Ce projet est sous license MIT</li>
-    </ul>
-</footer>
-</html>
